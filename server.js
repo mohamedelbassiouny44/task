676edb922acd8bdb5a3ec0fd02c19f94757771ca
mohamedelbassiouny44 <150030023+mohamedelbassiouny44@users.noreply.github.com>
2023-11-07 08:16:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ mongoose.connect("mongodb://0.0.0.0:27017/base")
     err=> {console.log(err);}
 );
 
-app.listen(3000, function(){
-    console.log('server now is open')
-})
+
 
 const student = new mongoose.Schema({
     name : String,
@@ -20,7 +18,7 @@ const student = new mongoose.Schema({
     age : Number,
     course : String    
 });
-let StudentModel= new mongoose.model('student', student);
+let StudentModel= new mongoose.model("student", student);
 
 
 
@@ -30,7 +28,7 @@ const course = new mongoose.Schema({
     price : Number,
     startingdate : Date    
 });
-let courseModel= new mongoose.model('course', course);
+let courseModel= new mongoose.model("course", course);
 
 
 let corse1 = new courseModel({
@@ -38,39 +36,44 @@ let corse1 = new courseModel({
     code : "124",
     price : 5500,
     startingdate : "1/1/2024"
-});
+}).save();
 
 let corse2 = new courseModel({
     name : "frontend",
     code : "125",
     price : 5000,
     startingdate : "1/1/2024"
-});
+}).save();
 
 let corse3 = new courseModel({
     name : "cyber security",
     code : "126",
     price : 10000,
     startingdate : "1/1/2024"
-});
+}).save();
 
 let st1 = new StudentModel({
     name : "mohamed elbassiouny",
     phone : "012121212",
     age : 21,
     course : "backend"
-});
+}).save();
 
 let st2 = new StudentModel({
     name : "mohamed ali",
     phone : "0128888",
     age : 21,
     course : "front end"
-});
+}).save();
 
 let st3 = new StudentModel({
     name : "momen ashraf",
     phone : "01288888",
     age : 21,
     course : "cyber security"
-});
+}).save();
+
+
+app.listen(3000, function(){
+    console.log('server now is open')
+})
